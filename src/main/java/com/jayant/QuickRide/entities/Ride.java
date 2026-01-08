4b865f06d963831213e616c4b06b82fcf8655cf2
Jayant Kumar <jayantkumar13ssm@gmail.com>
2026-01-08 20:22:06 +0530

    Added the Ride Entity

---
+++ src/main/java/com/jayant/QuickRide/entities/Ride.java
@@ -0,0 +1,44 @@
+package com.jayant.QuickRide.entities;
+
+import com.jayant.QuickRide.entities.enums.PaymentMethod;
+import com.jayant.QuickRide.entities.enums.RideRequestStatus;
+import com.jayant.QuickRide.entities.enums.RideStatus;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.locationtech.jts.geom.Point;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Ride {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "Geometry(Point,4326)")
+    private Point pickupLocation;
+
+    @Column(columnDefinition = "Geometry(Point,4326)")
+    private Point dropLocation;
+
+    @CreationTimestamp
+    private LocalDateTime createdTime;        // Driver accepts the ride
+
+    @ManyToOne(fetch = FetchType.LAZY)      // One rider can request Many rides
+    private Rider rider;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Driver driver;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentMethod paymentMethod;
+
+    @Enumerated(EnumType.STRING)
+    private RideStatus rideStatus;
+
+    private Double fare;
+
+    private LocalDateTime startedAt;      //driver starts the ride
+    private LocalDateTime endedAt;        //driver ended the ride
+}
