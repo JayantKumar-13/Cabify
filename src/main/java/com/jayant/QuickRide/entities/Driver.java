28ed7c9c8b7656b252bd15558bfaeda0f534eb2d
Jayant Kumar <jayantkumar13ssm@gmail.com>
2026-01-08 20:20:41 +0530

    Added the Driver Entity

---
+++ src/main/java/com/jayant/QuickRide/entities/Driver.java
@@ -0,0 +1,27 @@
+package com.jayant.QuickRide.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.locationtech.jts.geom.Point;
+
+@Entity
+@Getter
+@Setter
+public class Driver {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne                              // Each Driver is a single User
+    @JoinColumn(name = "user_id")                   // foreign Key
+    private User user;
+    private Double rating;
+    private Boolean available;
+
+    // Geometry for Hibernate to identify it as Spatial
+    @Column(columnDefinition = "Geometry(Point,4326)")     // 4326 -> We are dealing with earth locations
+    Point currentLocation;
+
+}
