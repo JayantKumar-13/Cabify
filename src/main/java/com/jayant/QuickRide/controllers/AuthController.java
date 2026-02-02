4520595f211c001eae1be93f8783c56966dd9294
Jayant Kumar <jayantkumar13ssm@gmail.com>
2026-02-02 18:38:53 +0530

    Added Refresh Token Api

--- src/main/java/com/jayant/QuickRide/controllers/AuthController.java
+++ src/main/java/com/jayant/QuickRide/controllers/AuthController.java
@@ -8,7 +8,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
 
 @RestController
 @RequestMapping("/auth")
@@ -22,6 +26,7 @@ public class AuthController {
         return new ResponseEntity<>(authService.signUp(signupDto), HttpStatus.CREATED);
     }
 
+    @Secured("ROLE_ADMIN")
     @PostMapping("/onBoardNewDriver/{userId}")
     ResponseEntity<DriverDto> onBoardNewDriver(@PathVariable Long userId, @RequestBody OnboardDriverDto onboardDriverDto) {
         return new ResponseEntity<>(authService.onboardNewDriver(userId,
@@ -40,4 +45,17 @@ public class AuthController {
             return ResponseEntity.ok(new LoginResponseDto(tokens[0]));
         }
 
+
+    @PostMapping("/refresh")
+    public ResponseEntity<LoginResponseDto> refresh(HttpServletRequest request) {
+        String refreshToken = Arrays.stream(request.getCookies()).
+                filter(cookie -> "refreshToken".equals(cookie.getName()))
+                .findFirst()
+                .map(Cookie::getValue)
+                .orElseThrow(() -> new AuthenticationServiceException("Refresh token not found inside the Cookies"));
+
+        String accessToken = authService.refreshToken(refreshToken);
+
+        return ResponseEntity.ok(new LoginResponseDto(accessToken));
+    }
 }
